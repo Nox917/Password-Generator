b349a4094b758766f5b0de1da778351ac5a75d86
Nox917 <mohamedalmustafa917@gmail.com>
2025-09-10 14:20:16 +0200

    modify render function
    I used an event Listener instead of an on click function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let pass1El = document.querySelector("#pass1")
-let pass2El = document.querySelector("#pass2")
+const pass1El = document.querySelector("#pass1")
+const pass2El = document.querySelector("#pass2")
+const generateBtnEl = document.querySelector("#gen-btn")
 
 function generatePassword(len){
     let randomPass = ""
@@ -13,14 +14,10 @@ function generatePassword(len){
     return randomPass
 }
 
-function renderPasswords(){
+generateBtnEl.addEventListener("click", function(){
     let inputValue = document.querySelector("#pass-len").value
+    pass1El.innerHTML = generatePassword(inputValue)
+    pass2El.innerHTML = generatePassword(inputValue)
     document.querySelector("#pass1").style.display = 'inline'
     document.querySelector("#pass2").style.display = 'inline'
-    pass1El.textContent = generatePassword(inputValue)
-    pass2El.textContent = generatePassword(inputValue)
-}
-
-console.log(generatePassword(15))
-
-
+})
